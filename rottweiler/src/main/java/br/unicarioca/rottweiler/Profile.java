52f30c2927e6b7c281b9810fef1ba04358b0c63b
fabio.oshiro <fabio.oshiro@9f0bb89a-f898-11dd-9d1a-b59b2e1864b6>
2010-04-20 04:27:56 +0000

    Persistindo?

--- rottweiler/src/main/java/br/unicarioca/rottweiler/Profile.java
+++ rottweiler/src/main/java/br/unicarioca/rottweiler/Profile.java
@@ -1,5 +1,19 @@
 package br.unicarioca.rottweiler;
 
-public class Profile {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Profile implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
 	private String uid;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
 }
--- rottweiler/src/test/java/br/unicarioca/rottweiler/RottweilerTest.java
+++ rottweiler/src/test/java/br/unicarioca/rottweiler/RottweilerTest.java
@@ -2,38 +2,60 @@ package br.unicarioca.rottweiler;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.thoughtworks.selenium.SeleneseTestCase;
 
 public class RottweilerTest extends SeleneseTestCase {
-	public static void main(String[] args) {
-		try {
-			RottweilerTest rott = new RottweilerTest();
-			rott.setUp();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+	EntityManager em;
 	public void setUp() throws Exception {
-		setUp("http://www.orkut.com.br/", "*chrome");
+		
+		// open/read the application context file
+	    ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("./META-INF/spring/applicationContext.xml");
+
+	    // instantiate our spring dao object from the application context
+	    EntityManagerFactory emf = (EntityManagerFactory)ctx.getBean("entityManagerFactory");
+	    em = emf.createEntityManager();
+	    setUp("http://www.orkut.com.br/", "*chrome");
 	}
 
 	public void testTeste() throws Exception {
 		selenium.open("/Main#Home");
 		selenium.windowMaximize();
 		System.out.println("Abra o navegador e digite a sua senha");
-		Thread.sleep(15000);
+		Thread.sleep(1000);
 		
 		//listando os amigos
-		selenium.selectFrame("orkutFrame");
-		selenium.click("link=Amigos");
-		selenium.waitForPageToLoad("30000");
-
+		boolean ok=false;
+		while(!ok){
+			try{
+				selenium.selectFrame("orkutFrame");
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			try{
+				selenium.click("link=Amigos");
+				selenium.waitForPageToLoad("30000");
+				ok = true;
+				Thread.sleep(1000);
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
 		List<LinkProfile> listLinkProfile=LinkProfile.findAll(selenium.getHtmlSource());
 		for (LinkProfile linkProfile : listLinkProfile) {
 			System.out.println("nome -> " + linkProfile.getNome());
 			System.out.println("uid -> " + linkProfile.getUid());
 			System.out.println("url -> " + linkProfile.getUrl());
 			System.out.println();
+			Profile profile = new Profile();
+			profile.setUid(linkProfile.getUid());
+			em.persist(profile);
+			
 			selenium.open(linkProfile.getUrl());
 			selenium.waitForPageToLoad("30000");
 			Thread.sleep(5000);
