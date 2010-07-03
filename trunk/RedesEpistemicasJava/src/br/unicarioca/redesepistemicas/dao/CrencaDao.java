e29d397185dbe86f2acf898598ae3138ff064c3a
fabio.oshiro <fabio.oshiro@9f0bb89a-f898-11dd-9d1a-b59b2e1864b6>
2010-07-03 01:25:43 +0000

    2 Inicio de salvar e recuperar conf de rede em XML

--- trunk/RedesEpistemicasJava/src/br/unicarioca/redesepistemicas/dao/CrencaDao.java
+++ trunk/RedesEpistemicasJava/src/br/unicarioca/redesepistemicas/dao/CrencaDao.java
@@ -6,15 +6,19 @@ public class CrencaDao {
 	public static String getXml(ParEpistemico parEpistemico){
 		StringBuilder a = new StringBuilder();
 		for(Double d:parEpistemico.getDoubleAntecedentes()){
-			a.append(d).append(",");
+			a.append(";").append(d);
 		}
 		StringBuilder c = new StringBuilder();
 		for(Double d:parEpistemico.getDoubleConsequentes()){
-			c.append(d).append(",");
+			c.append(";").append(d);
 		}
+		if(c.length()==0) c.append(";");
+		if(a.length()==0) a.append(";");
+		System.out.println("a " + a.toString());
+		System.out.println("c " + c.toString());
 		return "<crenca a=\"{a}\" c=\"{c}\" cid=\"{cid}\" nome=\"{nome}\" />"
-			.replace("{a}", a.toString().substring(0, a.length()-2))
-			.replace("{c}", c.toString().substring(0, a.length()-2))
+			.replace("{a}", a.toString().substring(1))
+			.replace("{c}", c.toString().substring(1))
 			.replace("{cid}", parEpistemico.getId()+"")
 			.replace("{nome}", parEpistemico.toString());
 	}
