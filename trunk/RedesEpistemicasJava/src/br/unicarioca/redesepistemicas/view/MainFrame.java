24fd2ce61ce0b9b09ce517522f27f3faf42d959c
fabio.oshiro <fabio.oshiro@9f0bb89a-f898-11dd-9d1a-b59b2e1864b6>
2010-08-27 00:24:10 +0000

    Grafico de Potencia

--- trunk/RedesEpistemicasJava/src/br/unicarioca/redesepistemicas/view/MainFrame.java
+++ trunk/RedesEpistemicasJava/src/br/unicarioca/redesepistemicas/view/MainFrame.java
@@ -3,8 +3,6 @@ package br.unicarioca.redesepistemicas.view;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
@@ -94,6 +92,11 @@ public class MainFrame extends JFrame implements InfoListener,WindowListener, Ci
 			public void pause() {}
 			public void criarFotografia() {}
 			public void continuar() {}
+		});
+		menuPrincipal.getGraficoPotencia().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				redeEpistemicaView.ligaDesligaGraficoPotencia();
+			}
 		});
 		menuPrincipal.getSalvar().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ev) {
